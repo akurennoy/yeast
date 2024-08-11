8984a03c8fe527526fc953820f0283ac6faf2f20
akurennoy <alexey.kurennoy@gmail.com>
2024-08-11 16:18:41 +0100

    add gavi10K; output ci

--- evaluate_on_real_world_data_2.R
+++ evaluate_on_real_world_data_2.R
@@ -3,7 +3,7 @@
 # We will randomly assign users that placed those orders to either control and
 # treatment and then apply the sequential tests to the resulting trajectory of
 # the cumulative revenue difference between the two groups. We will then repeat
-# this multiple times and measure the number of detections. 
+# this multiple times and measure the number of detections.
 # Since the users are assigned randomly post-factum and no real
 # treatment gets applied to the treatment group, the relative frequency of
 # detection should be close to the nominal significance level.
@@ -41,97 +41,103 @@ initialise_continuous_methods = function(robust_increment_std,
                                          non_robust_increment_std,
                                          expected_num_observations,
                                          actual_num_observations) {
-  return(list(
-    # -- YEAST
-    YEAST = YEAST$new(
-      "YEAST",
-      SIGNIFICANCE_LEVEL,
-      sum(expected_num_observations),
-      robust_increment_std
-    ),
-    YEASTnr = YEAST$new(
-      "YEAST-non-robust",
-      SIGNIFICANCE_LEVEL,
-      sum(expected_num_observations),
-      non_robust_increment_std
-    ),
-    # -- pYEAST
-    pYEAST14 = pYEAST$new(
-      "pYEAST14",
-      SIGNIFICANCE_LEVEL,
-      cumsum(expected_num_observations),
-      robust_increment_std,
-      cumsum(actual_num_observations)
-    ),
-    pYEAST14nr = pYEAST$new(
-      "pYEAST14-non-robust",
-      SIGNIFICANCE_LEVEL,
-      cumsum(expected_num_observations),
-      non_robust_increment_std,
-      cumsum(actual_num_observations)
-    ),
-    # -- mSPRT
-    mSPRTphi100 = mSPRT$new("mSPRT100", SIGNIFICANCE_LEVEL, robust_increment_std, 100),
-    mSPRTphi025 = mSPRT$new("mSPRT025", SIGNIFICANCE_LEVEL, robust_increment_std, 25),
-    mSPRTphi011 = mSPRT$new(
-      "mSPRT011",
-      SIGNIFICANCE_LEVEL,
-      robust_increment_std,
-      1 / 0.3 ^
-        2
-    ),
-    mSPRTphi100nr = mSPRT$new(
-      "mSPRT100-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      100
-    ),
-    mSPRTphi025nr = mSPRT$new(
-      "mSPRT025-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      25
-    ),
-    mSPRTphi011nr = mSPRT$new(
-      "mSPRT011-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      1 / 0.3 ^ 2
-    ),
-    # -- GAVI
-    GAVI250 = GAVI$new("GAVI250", SIGNIFICANCE_LEVEL, robust_increment_std, 250),
-    GAVI500 = GAVI$new("GAVI500", SIGNIFICANCE_LEVEL, robust_increment_std, 500),
-    GAVI750 = GAVI$new("GAVI750", SIGNIFICANCE_LEVEL, robust_increment_std, 750),
-    GAVI250nr = GAVI$new(
-      "GAVI250-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      250
-    ),
-    GAVI500nr = GAVI$new(
-      "GAVI500-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      500
-    ),
-    GAVI750nr = GAVI$new(
-      "GAVI750-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      750
-    ),
-    # -- CAA (Statsig)
-    CAA = CAA$new("CAA", SIGNIFICANCE_LEVEL, robust_increment_std),
-    CAAnr = CAA$new("CAA-non-robust", SIGNIFICANCE_LEVEL, non_robust_increment_std),
-    # -- Classical (a z-test conducted once at the end of the experiment)
-    Classical = Bonferroni$new("Classical", SIGNIFICANCE_LEVEL, robust_increment_std, 1),
-    Classicalnr = Bonferroni$new(
-      "Classical-non-robust",
-      SIGNIFICANCE_LEVEL,
-      non_robust_increment_std,
-      1
+  return(
+    list(
+      # -- YEAST
+      YEAST = YEAST$new(
+        "YEAST",
+        SIGNIFICANCE_LEVEL,
+        sum(expected_num_observations),
+        robust_increment_std
+      ),
+      YEASTnr = YEAST$new(
+        "YEAST-non-robust",
+        SIGNIFICANCE_LEVEL,
+        sum(expected_num_observations),
+        non_robust_increment_std
+      ),
+      # -- pYEAST
+      pYEAST14 = pYEAST$new(
+        "pYEAST14",
+        SIGNIFICANCE_LEVEL,
+        cumsum(expected_num_observations),
+        robust_increment_std,
+        cumsum(actual_num_observations)
+      ),
+      pYEAST14nr = pYEAST$new(
+        "pYEAST14-non-robust",
+        SIGNIFICANCE_LEVEL,
+        cumsum(expected_num_observations),
+        non_robust_increment_std,
+        cumsum(actual_num_observations)
+      ),
+      # -- mSPRT
+      mSPRTphi100 = mSPRT$new("mSPRT100", SIGNIFICANCE_LEVEL, robust_increment_std, 100),
+      mSPRTphi025 = mSPRT$new("mSPRT025", SIGNIFICANCE_LEVEL, robust_increment_std, 25),
+      mSPRTphi011 = mSPRT$new(
+        "mSPRT011",
+        SIGNIFICANCE_LEVEL,
+        robust_increment_std,
+        1 / 0.3 ^
+          2
+      ),
+      mSPRTphi100nr = mSPRT$new(
+        "mSPRT100-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        100
+      ),
+      mSPRTphi025nr = mSPRT$new(
+        "mSPRT025-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        25
+      ),
+      mSPRTphi011nr = mSPRT$new(
+        "mSPRT011-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        1 / 0.3 ^ 2
+      ),
+      # -- GAVI
+      GAVI250 = GAVI$new("GAVI250", SIGNIFICANCE_LEVEL, robust_increment_std, 250),
+      GAVI500 = GAVI$new("GAVI500", SIGNIFICANCE_LEVEL, robust_increment_std, 500),
+      GAVI750 = GAVI$new("GAVI750", SIGNIFICANCE_LEVEL, robust_increment_std, 750),
+      GAVI250nr = GAVI$new(
+        "GAVI250-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        250
+      ),
+      GAVI500nr = GAVI$new(
+        "GAVI500-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        500
+      ),
+      GAVI750nr = GAVI$new(
+        "GAVI750-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        750
+      ),
+      # -- CAA (Statsig)
+      CAA = CAA$new("CAA", SIGNIFICANCE_LEVEL, robust_increment_std),
+      CAAnr = CAA$new(
+        "CAA-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std
+      ),
+      # -- Classical (a z-test conducted once at the end of the experiment)
+      Classical = Bonferroni$new("Classical", SIGNIFICANCE_LEVEL, robust_increment_std, 1),
+      Classicalnr = Bonferroni$new(
+        "Classical-non-robust",
+        SIGNIFICANCE_LEVEL,
+        non_robust_increment_std,
+        1
+      )
     )
-  ))
+  )
 }
 
 
@@ -182,7 +188,7 @@ print(num_cores)
 
 clusterExport(
   cl,
-  varlist=c(
+  varlist = c(
     "DATA_DIRECTORY",
     "METRIC_COL",
     "USER_ID_COL",
@@ -293,39 +299,70 @@ process_file = function(i) {
 
 results = parLapply(cl, 2:length(input_files), process_file)
 
-# Stop the cluster
 stopCluster(cl)
 
 # Aggregating the results
 
-# List all files in the directory
 files = list.files(OUTPUT_DIRECTORY, full.names = TRUE)
 
-# Read and combine all CSV files into a single data.table
-df = rbindlist(lapply(files, function(file) fread(file) %>% .[, file := basename(file)]))
+df = rbindlist(lapply(files, function(file)
+  fread(file) %>% .[, file := basename(file)]))
 
-# Group by 'method' and calculate the mean detection rate
-fdr_dt = df[, .(detection_rate = mean(detection_rate, na.rm = TRUE)), by = method]
+fdr_dt = df[, .(num_detections = sum(num_detections),
+                num_trials = sum(num_trials)), by = method]
+num_methods = nrow(fdr_dt)
 
-# Add a 'variance_estimate' column based on the method name
+fdr_dt[, `:=`(detection_rate = num_detections / num_trials)]
 fdr_dt[, `:=`(
   variance_estimate = ifelse(grepl("non-robust$", method), "non-robust", "robust"),
-  method = str_split_fixed(method, "-", 2)[, 1]
+  method = str_split_fixed(method, "-", 2)[, 1],
+  ci_pm = qnorm(1 - 0.05 / 2 / num_methods) * sqrt(detection_rate * (1 - detection_rate) / num_trials)  # this is the CI half-length
 )]
 
-result_dt = dcast(fdr_dt, method ~ variance_estimate, value.var = "detection_rate")
+result_dt = dcast(fdr_dt,
+                  method ~ variance_estimate,
+                  value.var = c("detection_rate", "ci_pm"))
 
-# Round the values to 2 decimal places
 result_dt[, `:=`(
-  robust = round(robust, 2),
-  `non-robust` = round(`non-robust`, 2)
+  detection_rate_robust = round(detection_rate_robust, 4),
+  `detection_rate_non-robust` = round(`detection_rate_non-robust`, 4),
+  ci_pm_robust = round(ci_pm_robust, 4),
+  `ci_pm_non-robust` = round(`ci_pm_non-robust`, 4)
+)]
+
+result_dt[, `:=`(
+  robust = paste0(
+    sprintf("%.4f", detection_rate_robust),
+    " ± ",
+    sprintf("%.4f", ci_pm_robust)
+  ),
+  `non-robust` = paste0(
+    sprintf("%.4f", `detection_rate_non-robust`),
+    " ± ",
+    sprintf("%.4f", `ci_pm_non-robust`)
+  )
 )]
 
 methods = c(
-  "YEAST", "pYEAST14", "GAVI250", "GAVI500", "GAVI750",
-  "mSPRT100", "mSPRT011", "mSPRT025",
-  "GAVI250", "GAVI500", "GAVI750",
-  "CAA", "Classical"
+  "YEAST",
+  "pYEAST14",
+  "GAVI250",
+  "GAVI500",
+  "GAVI750",
+  "mSPRT100",
+  "mSPRT011",
+  "mSPRT025",
+  "GAVI250",
+  "GAVI500",
+  "GAVI750",
+  "GAVI10K",
+  "CAA"
 )
 
 print(result_dt[methods, c("method", "robust", "non-robust")])
+
+
+# library(xtable)
+#
+#
+# print(xtable(result_dt[methods, c("method", "robust", "non-robust")]))
